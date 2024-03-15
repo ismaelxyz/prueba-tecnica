05a58d043f4ed0948b981721a80456fd11bcc22d
ismaelxyz <ismaelbeli.com@gmail.com>
2024-03-15 15:39:13 -0400

    refactor(graphics): switch from Recharts to Nivo to improve charts visualisation
    The Recharts charts library was replaced by Nivo in order to optimise and improve the quality and functionality of the graphics visualisations in the project.

--- prueba-tecnica/client/src/app/components/ChartsTab.tsx
+++ prueba-tecnica/client/src/app/components/ChartsTab.tsx
@@ -1,132 +1,358 @@
-// import "./styles.css";
-import { Button, TooltipProps } from "@nextui-org/react";
+// install (please try to align the version of installed @nivo packages)
+// yarn add @nivo/bump
+import { ResponsiveBump } from "@nivo/bump";
 import React from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
-import {
-  ValueType,
-  NameType,
-} from "recharts/types/component/DefaultTooltipContent";
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    id: "Serie 1",
+    data: [
+      {
+        x: 2000,
+        y: 6,
+      },
+      {
+        x: 2001,
+        y: 3,
+      },
+      {
+        x: 2002,
+        y: 11,
+      },
+      {
+        x: 2003,
+        y: 10,
+      },
+      {
+        x: 2004,
+        y: 1,
+      },
+    ],
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    id: "Serie 2",
+    data: [
+      {
+        x: 2000,
+        y: 2,
+      },
+      {
+        x: 2001,
+        y: 10,
+      },
+      {
+        x: 2002,
+        y: 6,
+      },
+      {
+        x: 2003,
+        y: 12,
+      },
+      {
+        x: 2004,
+        y: 5,
+      },
+    ],
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    id: "Serie 3",
+    data: [
+      {
+        x: 2000,
+        y: 8,
+      },
+      {
+        x: 2001,
+        y: 12,
+      },
+      {
+        x: 2002,
+        y: 1,
+      },
+      {
+        x: 2003,
+        y: 1,
+      },
+      {
+        x: 2004,
+        y: 12,
+      },
+    ],
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    id: "Serie 4",
+    data: [
+      {
+        x: 2000,
+        y: 1,
+      },
+      {
+        x: 2001,
+        y: 8,
+      },
+      {
+        x: 2002,
+        y: 3,
+      },
+      {
+        x: 2003,
+        y: 6,
+      },
+      {
+        x: 2004,
+        y: 9,
+      },
+    ],
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    id: "Serie 5",
+    data: [
+      {
+        x: 2000,
+        y: 5,
+      },
+      {
+        x: 2001,
+        y: 9,
+      },
+      {
+        x: 2002,
+        y: 7,
+      },
+      {
+        x: 2003,
+        y: 9,
+      },
+      {
+        x: 2004,
+        y: 2,
+      },
+    ],
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    id: "Serie 6",
+    data: [
+      {
+        x: 2000,
+        y: 11,
+      },
+      {
+        x: 2001,
+        y: 11,
+      },
+      {
+        x: 2002,
+        y: 5,
+      },
+      {
+        x: 2003,
+        y: 2,
+      },
+      {
+        x: 2004,
+        y: 8,
+      },
+    ],
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    id: "Serie 7",
+    data: [
+      {
+        x: 2000,
+        y: 12,
+      },
+      {
+        x: 2001,
+        y: 6,
+      },
+      {
+        x: 2002,
+        y: 4,
+      },
+      {
+        x: 2003,
+        y: 11,
+      },
+      {
+        x: 2004,
+        y: 6,
+      },
+    ],
+  },
+  {
+    id: "Serie 8",
+    data: [
+      {
+        x: 2000,
+        y: 4,
+      },
+      {
+        x: 2001,
+        y: 7,
+      },
+      {
+        x: 2002,
+        y: 8,
+      },
+      {
+        x: 2003,
+        y: 5,
+      },
+      {
+        x: 2004,
+        y: 10,
+      },
+    ],
+  },
+  {
+    id: "Serie 9",
+    data: [
+      {
+        x: 2000,
+        y: 10,
+      },
+      {
+        x: 2001,
+        y: 4,
+      },
+      {
+        x: 2002,
+        y: 9,
+      },
+      {
+        x: 2003,
+        y: 3,
+      },
+      {
+        x: 2004,
+        y: 4,
+      },
+    ],
+  },
+  {
+    id: "Serie 10",
+    data: [
+      {
+        x: 2000,
+        y: 9,
+      },
+      {
+        x: 2001,
+        y: 2,
+      },
+      {
+        x: 2002,
+        y: 2,
+      },
+      {
+        x: 2003,
+        y: 7,
+      },
+      {
+        x: 2004,
+        y: 7,
+      },
+    ],
+  },
+  {
+    id: "Serie 11",
+    data: [
+      {
+        x: 2000,
+        y: 7,
+      },
+      {
+        x: 2001,
+        y: 1,
+      },
+      {
+        x: 2002,
+        y: 10,
+      },
+      {
+        x: 2003,
+        y: 8,
+      },
+      {
+        x: 2004,
+        y: 11,
+      },
+    ],
+  },
+  {
+    id: "Serie 12",
+    data: [
+      {
+        x: 2000,
+        y: 3,
+      },
+      {
+        x: 2001,
+        y: 5,
+      },
+      {
+        x: 2002,
+        y: 12,
+      },
+      {
+        x: 2003,
+        y: 4,
+      },
+      {
+        x: 2004,
+        y: 3,
+      },
+    ],
   },
 ];
 
-interface PayloadProps {
-  name: string;
-  unit: string;
-  value: number;
-}
-interface CustomTooltipProps {
-  label: string;
-  payload: PayloadProps[];
-  active: boolean;
-}
-const CustomTooltip = ({
-  active,
-  payload,
-  label,
-}: TooltipProps<ValueType, NameType>) => {
-  if (active && payload && payload.length) {
-    return (
-      <div className="p-4 bg-gray-100 flex flex-col gap-4 rounded-md">
-        <p className="text-medium text-lg">{label}</p>
-        <p className="text-sm text-blue-400">
-          Revenue:
-          <span className="ml-2">${payload[0].value}</span>
-        </p>
-        <p className="text-sm text-indigo-400">
-          Profit:
-          <span className="ml-2">${payload[1].value}</span>
-        </p>
-      </div>
-    );
-  }
-};
-
-export default function ChartsTab() {
+export default function ChartsTab(): React.ReactElement {
   return (
     <div className="flex flex-col items-center justify-center p-4 border border-slate-900 rounded-xl h-[400px]">
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip content={<CustomTooltip />} />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="pv"
-            stroke="#8884d8"
-            strokeDasharray="5 5"
-          />
-          <Line
-            type="monotone"
-            dataKey="uv"
-            stroke="#82ca9d"
-            strokeDasharray="3 4 5 2"
-          />
-        </LineChart>
-      </ResponsiveContainer>
+    <ResponsiveBump
+      data={data}
+      colors={{ scheme: "spectral" }}
+      lineWidth={3}
+      activeLineWidth={6}
+      inactiveLineWidth={3}
+      inactiveOpacity={0.15}
+      pointSize={10}
+      activePointSize={16}
+      inactivePointSize={0}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={3}
+      activePointBorderWidth={3}
+      pointBorderColor={{ from: "serie.color" }}
+      axisTop={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "",
+        legendPosition: "middle",
+        legendOffset: -36,
+        truncateTickAt: 0,
+      }}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "",
+        legendPosition: "middle",
+        legendOffset: 32,
+        truncateTickAt: 0,
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "ranking",
+        legendPosition: "middle",
+        legendOffset: -40,
+        truncateTickAt: 0,
+      }}
+      margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+      axisRight={null}
+    />
     </div>
   );
 }
