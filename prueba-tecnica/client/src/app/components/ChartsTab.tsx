3f1b62e4fd7274cc52bf5cfeac99f0fcf6b1c79c
ismaelxyz <ismaelbeli.com@gmail.com>
2024-03-15 14:31:52 -0400

    chore(graphics-library): verify implementation of chart libraries
    A review of the current implementation of the chart libraries in the project was carried out to ensure their correct integration and functioning, but will proceed to test with nivo.

--- prueba-tecnica/client/src/app/components/ChartsTab.tsx
+++ prueba-tecnica/client/src/app/components/ChartsTab.tsx
@@ -1,22 +1,132 @@
-"use client";
-
+// import "./styles.css";
+import { Button, TooltipProps } from "@nextui-org/react";
 import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  ValueType,
+  NameType,
+} from "recharts/types/component/DefaultTooltipContent";
 
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+interface PayloadProps {
+  name: string;
+  unit: string;
+  value: number;
+}
+interface CustomTooltipProps {
+  label: string;
+  payload: PayloadProps[];
+  active: boolean;
+}
+const CustomTooltip = ({
+  active,
+  payload,
+  label,
+}: TooltipProps<ValueType, NameType>) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="p-4 bg-gray-100 flex flex-col gap-4 rounded-md">
+        <p className="text-medium text-lg">{label}</p>
+        <p className="text-sm text-blue-400">
+          Revenue:
+          <span className="ml-2">${payload[0].value}</span>
+        </p>
+        <p className="text-sm text-indigo-400">
+          Profit:
+          <span className="ml-2">${payload[1].value}</span>
+        </p>
+      </div>
+    );
+  }
+};
 
 export default function ChartsTab() {
   return (
-    <LineChart width={900} height={300} data={data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="name" />
-      <YAxis />
-    </LineChart>
+    <div className="flex flex-col items-center justify-center p-4 border border-slate-900 rounded-xl h-[400px]">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            strokeDasharray="5 5"
+          />
+          <Line
+            type="monotone"
+            dataKey="uv"
+            stroke="#82ca9d"
+            strokeDasharray="3 4 5 2"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
-
-
-
-
